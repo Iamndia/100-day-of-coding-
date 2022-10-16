d40f6f933c3802ea04327204b4bfa9fdb5f69f46
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-10-17 06:56:10 +0800

    Day 1
    17 oct 2022
    Tipe data dan variabel

---
+++ day1.py
@@ -0,0 +1,10 @@
+nama = "Nadia"
+nim = "D0222022"
+prodi = "Teknik Informatika"
+umur = 18
+tinggi = 1.48
+status_pernikahan = False
+print(10*"=","BIODATA","="*10)
+print(" Hello Nama saya ",nama,"Dengan NIM ",nim,"Program studi ",prodi,".Umur saya ",umur,"dengan tinggi ",tinggi,"Status pernikahan ",status_pernikahan,"alias belum menikah.")
+print('')
+print(' Kenapa saya mengambil jurusan ',prodi,' karena saya mau memberbanyak pengetahuan saya tentang programming.')
