faaea23f1461b00368d72fbf02bc6704b2a1e9c5
Iamndia <115078535+Iamndia@users.noreply.github.com>
2022-11-02 18:42:19 +0800

    Add files via upload

---
+++ day15.py
@@ -0,0 +1,23 @@
+print('=====Def=====')
+#cara penggunaaan def
+def nama():
+	print('Nadia')
+	print('Sakura')
+	print('Hinata')
+nama()
+print()
+
+print(20*"==")
+#Def dengan menggunakan
+def segitiga(alas,tinggi):
+	luas = (alas * tinggi)/2
+	print('Luas = ',luas)
+segitiga(3,4)
+	
+print()
+print(20*"==")
+def bio(nama,umur,prodi,fakultas,univ):
+   print('Hello nama saya ',nama,'umur saya ',umur,'Prodi ',prodi,'Fakultas ',fakultas,'Universitas ',univ)
+bio('Nadia',18,'Informatika','Teknik','Sulawesi Barat')
+
+   
