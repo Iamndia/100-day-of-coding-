07eb31877773058f14d66a6d9f976066b899d8ef
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-11-13 19:59:26 +0800

    Add files via upload

---
+++ day28.py
@@ -0,0 +1,7 @@
+ket = 'Konversi bilangan'.center(40,'=')
+print(ket)
+dec = int(input('Masukkan nilai desimal = '))
+print(f'hasil konversi {dec} ke biner adalah {bin(dec)[2:]}')
+print(f'hasil konversi {dec} ke oktal adalah {oct(dec)[2:]}')
+print(f'hasil konversi {dec} ke hexadesimal adalah {hex(dec)[2:]}\n')
+
