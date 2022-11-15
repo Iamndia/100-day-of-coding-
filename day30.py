5babf600f1e66edc444bc060575ab794c8fe4bb0
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-11-15 20:14:28 +0800

    Add files via upload

---
+++ day30.py
@@ -0,0 +1,21 @@
+#for loop
+
+#dengan string
+data = 'nadia cantik'
+for bio in data:
+	print(bio)
+#dengan matriks	
+mat =[[2,5],[7,2]]
+for i in mat:
+	print(i)
+#dengan range
+for my in range(1,10):
+	print(f' data -> {my}')
+print()	
+			
+#while loop
+
+nilai = 0
+while nilai < 10:
+	nilai += 1
+	print(f'Nadia -> {nilai}')
