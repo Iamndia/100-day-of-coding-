d1e4579b2d48a41b70858c88686a072f9a8a7260
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-11-19 18:21:17 +0800

    Add files via upload

---
+++ day34.py
@@ -0,0 +1,10 @@
+nama = input('Masukkan huruf random = ')
+list = [i for i in nama]
+print(f'list = {list}')
+if 'o' and 'u' and 'y' and 'f' and 'c' and 'a' in list:
+	w= ['a','i','c','u','h','r','s']
+	print(
+	)
+	print(w[1],' ',w[2],w[5],w[3],w[6],w[4],' ',w[3])
+else:
+	print('nothing,,,,,')
