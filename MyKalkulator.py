52df4aeba1ab2f306e3b052d42730925aa8dee62
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-12-21 19:43:20 +0800

    day 66

---
+++ MyKalkulator.py
@@ -0,0 +1,26 @@
+print('=' * 25)
+print('Operasi Matematika')
+print('  1. Jumlah \t [+]')
+print('  2. Kurang \t [-]')
+print('  3. Kali \t [*]')
+print('  4. Bagi \t [/]')
+print('=' * 25)
+
+operasi = input('Pilih operasi (1/2/3/4): ')
+bilangan_1 = eval(input('Masukkan bilangan pertama: '))
+bilangan_2 = eval(input('Masukkan bilangan kedua: '))
+
+if operasi == '1':
+  print(f" hasil = {bilangan_1 + bilangan_2}")
+  print('User memilih penjumlahan')
+elif operasi == '2':
+  print(f"hasil = {bilangan_1 - bilangan_2}")
+  print('User memilih pengurangan')
+elif operasi == '3':
+  print(f"hasil = {bilangan_1 * bilangan_2}")
+  print('User memilih perkalian')
+elif operasi == '4':
+  print(f"hasil = {bilangan_1 / bilangan_2}")
+  print('User memilih pembagian')
+else:
+  print('Tidak valid')
