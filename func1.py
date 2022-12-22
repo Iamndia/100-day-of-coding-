f4d0a183d5afa253778cf61167b28ca8705f8c7f
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-12-22 19:47:28 +0800

    Add files via upload

---
+++ func1.py
@@ -0,0 +1,20 @@
+#fungsi biasa
+def nama():
+    print("Nama saya Nadia")
+
+def umur():
+    print("Umur saya 18")
+
+def tinggi():
+    print("Tinggi saya 1,38")
+
+def bio():
+    print("Nama saya Nadia")
+    print("umur saya  18")
+    print("tinggi 1.38")
+
+bio()
+
+nama()
+umur()
+tinggi()
