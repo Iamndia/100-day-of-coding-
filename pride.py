45996f5dd04c409be68317d82d7ddabb60f0727e
Nadia <115078535+Iamndia@users.noreply.github.com>
2022-12-28 07:16:00 +0800

    Add files via upload

---
+++ pride.py
@@ -0,0 +1,11 @@
+nilai = int(input("Nilai = "))
+# for i in range(1,nilai+1):
+if  nilai % 3 == 0 and nilai % 5 == 0:
+        print("master class")
+elif nilai % 5 == 0:
+        print("pride of 5")
+elif nilai % 3 == 0:
+    print("pride of 3")
+
+else:
+        print()
