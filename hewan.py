5ad1c92353674de3dcad172597fe20a2174d7140
Nadia <115078535+Iamndia@users.noreply.github.com>
2023-01-01 14:37:05 +0800

    Add files via upload

---
+++ hewan.py
@@ -0,0 +1,11 @@
+hewan = ["bebek","kamibng","ayam","burung","kucing","unta","ular","gajah","kecoa","keong"]
+print(f"hewan = {hewan}")
+hewan3 = hewan[3]
+hewan5 = hewan[5]
+hapus = hewan.remove("ular")
+tambah = hewan.append("kodok")
+tambah1 = hewan.append("katak")
+print(f"elemen 3 = {hewan3}")
+print(f"elemen 5 = {hewan5}")
+print(f"hapus = {hewan}")
+print(f"setelah ditambah = {hewan}")
