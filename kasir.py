2403bd7f229a1193b32b051f40d97a4f4188827c
Nadia <115078535+Iamndia@users.noreply.github.com>
2023-01-03 12:10:02 +0800

    Add files via upload

--- kasir.py
+++ kasir.py
@@ -2,45 +2,31 @@ def line():
     print("="*50)
 def title(judul):
     print(judul.center(50))
-line()
-title("program kasir")
-line()
-print("""
-Produk              |       Harga
-----------------------------------
-1. Mie sedap        |       4.000
-2. Gula Pasir       |       6.000
-3. Jalankote        |       2.000
-4. Mie soto         |       4.000
-5. Mie kaldu        |       4.000
-""")
+def footer():
+    print("Terima kasih telah berkunjung di Nadia cell".center(50))
+def header():
+    print("Nadia Cell".center(50))
+def line1():
+    print("-"*50)
 
+line1()
+header()
+line1()
+jum_barang = int(input("Jumlah belanja = "))
 line()
-jumlah_barang = int(input("Jumlah barang\t\t= "))
-for i in range(1,jumlah_barang+1):
-    barang_belanja = int(input("masukkan nomor barang\t= "))
-    if barang_belanja == 1:
-        harga = 4000
-        nama = "mie sedap"
-    elif barang_belanja == 2:
-        harga = 6000
-        nama = "Gula pasir"
-    elif barang_belanja == 3:
-        harga = 2000
-        nama = "jalan kote"
-    elif  barang_belanja == 4:
-        harga = 4000
-        nama = "mie soto"
-    elif barang_belanja == 5:
-        harga = 4000
-        nama = "mie kaldu"
-    else:
-        print("Produk tidak aada")
-    print(f"Detail\t\t\t= {barang_belanja} | {harga}")
-line()
-total =  barang_belanja*harga
-print(f"total harga = {total}")
-       
-
-
+for i in range(1,jum_barang+1):
+    nm_barang = input("Nama barang\t\t = ")
+    jumlah = int(input("Jumlah barang\t\t = "))
+    harga_barang = int(input("Harga barang\t\t = "))
+    harga = harga_barang*jumlah
     
+    line1()
+    print(f"{nm_barang}\t|\t{jumlah}\t|\t{harga_barang}")
+    line1()
+    print(f"Total Harga\t\t = {harga} ")
+    line()
+footer()
+
+
+
+
