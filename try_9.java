cef0077c99f6f5709d5ad8a2b2d2c1999d102dd7
Nadia <115078535+Iamndia@users.noreply.github.com>
2023-01-17 10:56:09 +0800

    Add files via upload

---
+++ try_9.java
@@ -0,0 +1,16 @@
+
+package try1;
+
+//mengubah huruf kapital dan kecil
+
+public class try_9 {
+    public static void main(String[] args) {
+        String str = "NadIa";
+        String strUp = str.toUpperCase();
+        String strLow = str.toLowerCase();
+        System.out.println("Awal         = "+str);
+        System.out.println("diUpper      = "+strUp);
+        System.out.println("diLower      = "+strLow);
+    }
+    
+}
